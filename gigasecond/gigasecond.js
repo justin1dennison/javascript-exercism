a6e438d158dc25e62b04bb47b69760edbcaebe4b
Justin Dennison <justin1dennison@gmail.com>
2019-08-07 10:52:55 -0400

    went with a more straight forward solution

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,12 +1,1 @@
-const GIGASECOND = 1E9
-const MILLISECONDS_PER_SECOND = 1E9
-const add = x => y => x + y
-const totalMilliSecondsSinceEpoch = date => date.getTime()
-const dateFromMilliSecondsSinceEpoch = ms => new Date(ms)
-const pipe = (...fns) => val => fns.reduce((acc, fn) => fn(acc), val)
-
-export const gigasecond = pipe(
-  totalMilliSecondsSinceEpoch, 
-  add(GIGASECOND * MILLISECONDS_PER_SECOND),
-  dateFromMilliSecondsSinceEpoch
-)
+export const gigasecond = date => new Date(date.getTime() + 1E12)
