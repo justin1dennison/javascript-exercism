7429ea913e7b3b574eabeb35df4ca23e0d92ecf1
Justin Dennison <justin1dennison@gmail.com>
2019-10-07 11:09:17 -0400

    refactor patterns for regex lookups

--- bob/bob.js
+++ bob/bob.js
@@ -1,6 +1,9 @@
-const punctuation = '[!@#$%^&*():;\'",.<>/?]'
-const removePunctuation = s => s.replace(new RegExp(punctuation, 'ig'), '')
-const removeSpaces = s => s.replace(/ /ig, '')
+const punctuation = /[!@#$%^&*():;'",.<>/?]/g
+const space = / /g
+const question = /\?$/
+const empty = ''
+const removePunctuation = s => s.replace(punctuation, empty)
+const removeSpaces = s => s.replace(space, empty)
 const asCharCodes = s => [...s].map((_, i) => s.charCodeAt(i))
 const justNumbers = s => asCharCodes(removeSpaces(s))
 	.every(c => c <= 57 && c >= 48)
@@ -8,7 +11,7 @@ const isShouting = message => {
     const transformed = removePunctuation(message.trim())
     return transformed.toUpperCase() === transformed && !justNumbers(transformed)
 }
-const isQuestion = message => /\?$/g.test(message.trim()) 
+const isQuestion = message => question.test(message.trim()) 
 const isSilent = message => message.trim().length === 0
 export const hey = message => {
     if(isSilent(message))
